21a24b37f1d2104c66543bcbff42dacf2a1e3e2d
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2021-01-27 05:42:15 -0500

    Update MessageEvent.js

--- Discord_bot/MessageEvent.js
+++ Discord_bot/MessageEvent.js
@@ -1,12 +1,29 @@
 const prefix = process.env.botPrefix;
 const users = process.s;
+const helpMessage = 
+`
+\`${prefix}help\` - See how you use me.
+\`${prefix}register\` - Register and get your API key.
+\`${prefix}info\` -  Get your API key.
+\`${prefix}stats\` - See your request stats.
+`
 module.exports = async (bot, msg) => {
+
+    if(msg.content == `<@${bot.user.id}>` || msg.content == `<@!${bot.user.id}>`) {
+        msg.channel.createMessage(helpMessage);
+    };
 
     if(!msg.content.startsWith(prefix) || msg.author.bot || !msg.channel.guild) return;
 
     let args = msg.content.split(/ +/)
     const cmd = args[0].toLowerCase().slice(prefix.length);
     args = args.slice(1);
+
+    
+
+    if(cmd == 'help') {
+        msg.channel.createMessage(helpMessage);
+    };
 
     if(cmd == 'register') {
         let userData = await users.getID(msg.author.id);
@@ -21,4 +38,20 @@ module.exports = async (bot, msg) => {
         });
     };
 
+    if(cmd == 'info') {
+        const userData = await users.getID(msg.author.id);
+        if(!userData) return msg.channel.createMessage(`You are not registered, \`${prefix}register\``);
+        const message = await msg.channel.createMessage('I will DM you your info!');
+        bot.getDMChannel(msg.author.id).then(c => {
+            c.createMessage(`**___MonkeDev - API___**\nID: \`${userData.id}\`\nKEY: \`${userData.key}\``).catch(() => {
+                message.edit('Please enable your DMs so I can dm you your info!')
+            });
+        });
+    };
+
+    if(cmd == 'stats') {
+        const userData = await users.getID(msg.author.id);
+        if(!userData) return msg.channel.createMessage(`You are not registered, \`${prefix}register\``);
+        msg.channel.createMessage(`You have made a request to our API ${userData.stats.total} time(s)`);
+    };
 }
