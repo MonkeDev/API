01fd17405fbd6ef4d1cfe7a72bfc02f0bb347943
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2021-01-30 16:04:10 -0500

    Update Other.js

--- Routes/Other.js
+++ Routes/Other.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 
 
 router.post('/top.gg_vote', (req, res) => {
-    console.log('BODY \n' + req.body);
+    console.log('BODY');
+    console.log(req.body);
 
-    console.log('HEADERS \n' + req.headers);
+    console.log('HEADERS');
+    console.log(req.headers);
 });
 
 
