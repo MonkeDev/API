8ef973a168d25509cd51126ff38c81ef4b6276a7
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2021-01-30 16:48:00 -0500

    Update Other.js

--- Routes/Other.js
+++ Routes/Other.js
@@ -1,9 +1,74 @@
 const router = require('express').Router();
-
+const { Client } = require('eris');
+const bot = new Client();
 
 
 
 router.post('/top.gg_vote', (req, res) => {
+
+    console.log(req.body)
+
+    const webhookID = req.urlParams.webhookID;
+    if(!webhookID) return res.status(400).json({
+        error: true,
+        message: 'A webhookID was not provided.'
+    });
+    const webhookToken = req.urlParams.webhookToken;
+    if(!webhookToken) return res.status(400).json({
+        error: true,
+        message: 'A webhookToken was not provided.'
+    });
+    
+    let message;
+    if(req.urlParams.message) {
+        message = req.urlParams.message;
+    } else {
+        message = { 
+            content: req.urlParams.message || `<@!$USER_ID>, Thank you for voting for <@!$BOT_ID>!`
+        }
+    }
+
+    const toReplace = [
+        {
+            in: '$USER_ID',
+            out: req.body.user
+        },
+        {
+            in: '$BOT_ID',
+            out: req.body.bot
+        },
+        {
+            in: '$IS_WEEKEND',
+            out: req.body.isWeekend
+        }
+    ];
+
+    try {
+
+        if(message === req.urlParams.message && typeof message == 'object') {
+            toReplace.forEach(x => {
+                message.content = message.content.split(x.in).join(x.out);
+            });
+        } else {
+            toReplace.forEach(x => {
+                message.content = message.content.split(x.in).join(x.out);
+            });
+        };
+
+        bot.executeWebhook(webhookID, webhookToken, message);
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({
+            error: true,
+            message: `${err}`
+        });
+    };   
+    
+    
+    
+
+    /* 
+
     console.log('BODY');
     console.log(req.body);
 
@@ -11,6 +76,7 @@ router.post('/top.gg_vote', (req, res) => {
     console.log(req.headers);
 
     res.send('ok');
+    */
 });
 
 
