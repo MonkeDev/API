db1aeefd81922de947ba0e1eec861f441ee2c685
Connor <halil_ismail@yahoo.com>
2021-05-27 17:02:50 +0200

    fix the same woopsies again

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -29,7 +29,7 @@ router.post('/add', async (req, res) => {
 router.post('/register', async (req, res) => {
 
     const userID = req.urlParams.userID;
-    if (!userID) return req.json({ error: true, message: 'no userID' });
+    if (!userID) return res.json({ error: true, message: 'no userID' });
 
     const users = process.s;
 
@@ -48,7 +48,7 @@ router.get('/user/info', async (req, res) => {
 
     const userID = req.urlParams.userID;
 
-    if (!userID) return req.json({ error: true, message: 'no userID' });
+    if (!userID) return res.json({ error: true, message: 'no userID' });
 
     const users = process.s;
 
